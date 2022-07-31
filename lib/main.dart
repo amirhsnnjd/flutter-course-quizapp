e77f90a2c133b80f468bb1a6948661cba3408baa
amir <amirhsnnjd@yahoo.com>
2022-07-31 13:44:10 +0430

    Button Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
- 
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: Text('Hello flutter!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('hello'),
+        ),
+        body: Column(children: [
+          Text('question ? '),
+          Row(children: [
+            RaisedButton( child: Text('ANSWER1'), onPressed: null),
+          RaisedButton( child: Text('ANSWER2'), onPressed: null),
+          RaisedButton( child: Text('ANSWER3'), onPressed: null),
+          ],),
+          RaisedButton( child: Text('answer1'), onPressed: null),
+          RaisedButton( child: Text('answer2'), onPressed: null),
+          RaisedButton( child: Text('answer3'), onPressed: null),
+        
+
+        ]),
+      ),
     );
   }
 }
